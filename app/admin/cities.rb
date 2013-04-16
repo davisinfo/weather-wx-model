d807dd7a3ec8f34c2a16841b67f50ba1dba35271
Andrei Latchescu <andrei@nextbyte.ro>
2013-04-16 09:28:21 +0300

    fix update from cities page

--- app/admin/cities.rb
+++ app/admin/cities.rb
@@ -18,12 +18,28 @@ ActiveAdmin.register City do
 		city = City.find(params[:id])
 		aw = Accuweather.new
 		data = aw.get_weather_data(city)
-		data.each do |_wd|
-			wd = WeatherData.find_or_create_by_city_id_and_date(city.id,_wd[:date])
-			wd.high = _wd[:high]
-			wd.low = _wd[:low]
-			wd.save
-		end
+	    data.each do |_wd|
+	      wd = WeatherData.find_or_create_by_city_id_and_date_and_computed_on_and_is_forecast(city.id,_wd[:date], computed_on,_wd[:is_forecast])
+	      wd.high = _wd[:high]
+	      wd.low = _wd[:low]
+	      wd.computed_on = computed_on
+	      _avg = (_wd[:high]*1.00 + _wd[:low]*1.00) / 2.00
+	      wd.cdd = 0
+	      wd.hdd = 0
+	      if _avg > 65 then
+	        wd.cdd = _avg - 65.00
+	      else
+	        wd.hdd = 65.00 - _avg
+	      end
+	      wd.tdd = wd.cdd + wd.hdd
+	      wd.is_forecast = _wd[:is_forecast]
+	      wd.save
+	      wd.get_week
+	      _wds = WeatherData.find_all_by_date_and_is_forecast(_wd[:date],true,:order => "computed_on desc")
+	      5.upto(_wds.count) do |i|
+	        _wds[i].destroy unless _wds[i].nil?
+	      end
+	    end
 		
 		redirect_to admin_cities_path and return
 	end
@@ -39,27 +55,28 @@ ActiveAdmin.register City do
 		computed_on = Date.today
 		City.all.each do |city|		
 			data = aw.get_weather_data(city)
-			data.each do |_wd|
-				wd = WeatherData.find_or_create_by_city_id_and_date_and_computed_on(city.id,_wd[:date], computed_on)
-				wd.high = _wd[:high]
-				wd.low = _wd[:low]
-				wd.computed_on = computed_on
-				_avg = (_wd[:high] + _wd[:low]) / 2.00
-				wd.cdd = 0
-				wd.hdd = 0
-				if _avg > 65 then
-					wd.cdd = _avg - 65
-				else
-					wd.hdd = 65 - _avg
-				end
-				wd.tdd = wd.cdd + wd.hdd
-				wd.is_forecast = _wd[:is_forecast]
-				wd.save
-				_wds = WeatherData.find_all_by_date_and_is_forecast(_wd[:date],true,:order => "computed_on desc")
-				5.upto(_wds.count) do |i|
-					_wds[i].destroy unless _wds[i].nil?
-				end
-			end
+		    data.each do |_wd|
+		      wd = WeatherData.find_or_create_by_city_id_and_date_and_computed_on_and_is_forecast(city.id,_wd[:date], computed_on,_wd[:is_forecast])
+		      wd.high = _wd[:high]
+		      wd.low = _wd[:low]
+		      wd.computed_on = computed_on
+		      _avg = (_wd[:high]*1.00 + _wd[:low]*1.00) / 2.00
+		      wd.cdd = 0
+		      wd.hdd = 0
+		      if _avg > 65 then
+		        wd.cdd = _avg - 65.00
+		      else
+		        wd.hdd = 65.00 - _avg
+		      end
+		      wd.tdd = wd.cdd + wd.hdd
+		      wd.is_forecast = _wd[:is_forecast]
+		      wd.save
+		      wd.get_week
+		      _wds = WeatherData.find_all_by_date_and_is_forecast(_wd[:date],true,:order => "computed_on desc")
+		      5.upto(_wds.count) do |i|
+		        _wds[i].destroy unless _wds[i].nil?
+		      end
+		    end
 		end
 		
 		redirect_to admin_cities_path and return
